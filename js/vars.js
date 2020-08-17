5b58fc5c0eee3ed819f6b327eccad3dc65d1456d
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2020-08-17 21:44:54 +0300

    input.js

---
+++ js/vars.js
@@ -0,0 +1,15 @@
+const age = 10;
+const totalPrice = 200.74;
+const userName = "Chelsy";
+const message = "Добро пожаловать!";
+const isOpen = true;
+const shouldConfirm = false;
+
+// console.log(true);
+
+const type = typeof true;
+console.log(type);
+
+// console.log("Before");
+// alert("qwe");
+// console.log("After");
