80f358a81e6212e3f8ae09b867457d1f56603e5a
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2020-08-17 22:11:54 +0300

    number.js

---
+++ js/input.js
@@ -0,0 +1,8 @@
+// const message = "Do want renew?";
+// const shouldRenew = confirm(message);
+// console.log(shouldRenew);
+
+// let quantity = prompt("Введите количество товаров");
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
